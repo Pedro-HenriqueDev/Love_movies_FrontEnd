38ebf4af06d1f65a287eff879dceafe30b8f0ae3
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-05-04 18:09:20 -0300

    excluir user funcionando

--- src/components/modalUser.vue
+++ src/components/modalUser.vue
@@ -12,23 +12,18 @@
         <img class="w-16 rounded-full" src="../assets/profile-picture-5.jpg" alt="Rounded avatar">
         <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white"></h5>
         <span class="text-sm text-gray-500 dark:text-gray-400"></span>
-        <div class="flex mt-4 space-x-3 md:mt-6">
-            <button @click="erasedCookie" type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Sair</button>
-            <button @click="openInput" type="button" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Apagar Conta</button>
-        </div>
-        <div v-show="pass">
-            <div>
-            <div class="relative w-full">
-                <label for="default-search" class="mb-2 mt-10 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                <input type="password" v-model="password" id="default-search" class="block w-52 p-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" required>
-                <button @click="erasedAccout" type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-red-700 hover:bg-red-800 font-medium rounded-lg text-sm px-3 py-1 dark:bg-red-600 dark:hover:bg-red-700">Apagar</button>
+        <div class="flex flex-col mt-4 space-x-3 md:mt-6">
+            <div class="flex">
+                <button @click="erasedCookie" type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Sair</button>
+                <button @click="pass? pass = false: pass = true" type="button" class="text-gray-900 bg-red-600 border focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:text-white dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Apagar Conta</button>
+            </div>
+            <div v-if="pass">
+                <input type="password" v-model="password" id="default-search" class="block w-52 p-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Senha" required>
+                <button @click="erasedAccout" type="button" class="text-gray-900 bg-red-600 border focus:outline-none hover:bg-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:text-white dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Apagar</button>
                
             </div>
-            <p class="text-white">{{ messageError }}</p>
-</div>
-            
         </div>
-    </div>
+        </div>
     
 </div>
     
@@ -41,6 +36,7 @@ export default {
     data() {
         let messageError = ""
         return {
+            email: "",
             messageError,
             password: "",
             pass: false,
@@ -72,11 +68,13 @@ export default {
         },
         erasedAccout() {
             let token = document.cookie.split("=")
-            
+            console.log(token[1])
+            console.log(this.password)
             axios.delete(process.env.VUE_APP_URL_APIUSER + "users", {
-                data: {password: this.password},
-                headers: {Authorization: `Bearer ${token[1]}`}
-            }).then(res => {
+                    data: {password: this.password},
+                    headers: {"Authorization": `Bearer ${token[1]}`}
+                }).then(res => {
+                    console.log(res)
                 if(res.status == 200) {
                     this.erasedCookie()
                     this.$router.push("/")
